40aa3c23160acd430a341a0f596e7762f781f5df
nodes777 <nodes777@gmail.com>
2016-01-05 13:08:09 -0500

    initial commit for udcaity review

--- js/app.js
+++ js/app.js
@@ -1,84 +1,63 @@
-/* STUDENTS IGNORE THIS FUNCTION
- * All this does is create an initial
- * attendance record if one is not found
- * within localStorage.
- */
-(function() {
-    if (!localStorage.attendance) {
-        console.log('Creating attendance records...');
-        function getRandom() {
-            return (Math.random() >= 0.5);
+
+
+//model
+var model = {
+    thisStudent: null,
+    students: [
+        {
+            name: "Slappy",
+            daysMissed: 0
         }
-
-        var nameColumns = $('tbody .name-col'),
-            attendance = {};
-
-        nameColumns.each(function() {
-            var name = this.innerText;
-            attendance[name] = [];
-
-            for (var i = 0; i <= 11; i++) {
-                attendance[name].push(getRandom());
-            }
-        });
-
-        localStorage.attendance = JSON.stringify(attendance);
+    ]
+};
+console.log(model.students[0]);
+//octo
+var octo = {
+    init: function(){
+        //model.thisStudent = model.students[0];
+        view.init();
+    },
+    getStudents: function(){
+        return model.students;
+    },
+    incrementDaysMissed: function(){
+        model.thisStudent.daysMissed++;
+        view.render();
     }
-}());
+};
 
+var view = {
+    init: function(){
+       //this.studentTable = document.getElementById("taylorStudent");
+       this.render();
+    },
+    render: function(){
+        var tbl = document.createElement('table');
+        tbl.style.width = '100%';
+        tbl.setAttribute('border', '1');// creates table and styles
+        var tbdy = document.createElement('tbody');//creates table body
 
-/* STUDENT APPLICATION */
-$(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+        var  elem, i, thisStudent, j;// declares variables
+        var students = octo.getStudents();//gets students from octo
+        //
+        var tayTableDiv = document.getElementById("tayTable");
 
-    // Count a student's missed days
-    function countMissing() {
-        $allMissed.each(function() {
-            var studentRow = $(this).parent('tr'),
-                dayChecks = $(studentRow).children('td').children('input'),
-                numMissed = 0;
-
-            dayChecks.each(function() {
-                if (!$(this).prop('checked')) {
-                    numMissed++;
+        for(i=0; i<students.length; i++){//for each student
+            var tr = document.createElement('tr');//creates a new row for each student
+                while(j<12){
+                    var td = document.createElement('td');//creates a cell
+                    td.appendChild(document.createTextNode('test'));
+                    tr.appendChild(td);// add cell to that row
+                    j++//until you have 12 cells
                 }
-            });
 
-            $(this).text(numMissed);
-        });
+        tbdy.appendChild(tr);// add each student row to the table body
+        }
+        tbl.appendChild(tbdy);//add the table body to the table
+        tayTableDiv.appendChild(tbl);//add the table to the tayTableDiv
     }
+};
 
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
 
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
-        });
-    });
-
-    // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
-        var studentRows = $('tbody .student'),
-            newAttendance = {};
-
-        studentRows.each(function() {
-            var name = $(this).children('.name-col').text(),
-                $allCheckboxes = $(this).children('td').children('input');
-
-            newAttendance[name] = [];
-
-            $allCheckboxes.each(function() {
-                newAttendance[name].push($(this).prop('checked'));
-            });
-        });
-
-        countMissing();
-        localStorage.attendance = JSON.stringify(newAttendance);
-    });
-
-    countMissing();
-}());
+octo.init();
+view.render();
