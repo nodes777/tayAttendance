90f5a0bd0b64be58e363e75386e238e4df1802d4
nodes777 <nodes777@gmail.com>
2016-01-08 10:05:51 -0500

    builds table, trying to add event listener

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 //model
 var model = {
     thisStudent: null,
@@ -7,57 +5,70 @@ var model = {
         {
             name: "Slappy",
             daysMissed: 0
+        },
+        {
+            name: "Lily",
+            daysMissed: 0
         }
     ]
 };
 console.log(model.students[0]);
-//octo
+
 var octo = {
-    init: function(){
+    init: function() {
         //model.thisStudent = model.students[0];
         view.init();
     },
-    getStudents: function(){
+    getStudents: function() {
         return model.students;
     },
-    incrementDaysMissed: function(){
+    incrementDaysMissed: function(thisStudent) {
         model.thisStudent.daysMissed++;
         view.render();
     }
 };
 
 var view = {
-    init: function(){
-       //this.studentTable = document.getElementById("taylorStudent");
-       this.render();
+    init: function() {
+        this.render();
     },
-    render: function(){
-        var tbl = document.createElement('table');
+    render: function() {
+        var tbl = document.createElement('table'); // creates table and styles
         tbl.style.width = '100%';
-        tbl.setAttribute('border', '1');// creates table and styles
-        var tbdy = document.createElement('tbody');//creates table body
+        tbl.setAttribute('border', '1');
+        var tbdy = document.createElement('tbody');
 
-        var  elem, i, thisStudent, j;// declares variables
-        var students = octo.getStudents();//gets students from octo
-        //
-        var tayTableDiv = document.getElementById("tayTable");
+        var students = octo.getStudents(); //gets students from octo
+        var tayTableDiv = document.getElementById("tayTable");// gets table div from DOM
 
-        for(i=0; i<students.length; i++){//for each student
-            var tr = document.createElement('tr');//creates a new row for each student
-                while(j<12){
-                    var td = document.createElement('td');//creates a cell
-                    td.appendChild(document.createTextNode('test'));
-                    tr.appendChild(td);// add cell to that row
-                    j++//until you have 12 cells
-                }
-
-        tbdy.appendChild(tr);// add each student row to the table body
+        for (var i = 0; i < students.length; i++) { //for each student
+            var thisStudent = students[i];
+            var tr = document.createElement('tr');
+            var studentName = document.createElement('td')
+            studentName.appendChild(document.createTextNode(students[i].name));
+            tr.appendChild(studentName); //creates a new row for each student
+            for (var j = 0; j < 12; j++) { //create the check boxes
+                var td = document.createElement('td'); //creates a cell
+                var newCheckBox = document.createElement('input');
+                newCheckBox.type = 'checkbox';
+                td.appendChild(newCheckBox); //add checkbox to cell
+                tr.appendChild(td); // add cell to that row
+                newCheckBox.addEventListener('click', (function(thisStudent) {//add event listener to each checkbox
+                    return function() {
+                        octo.incrementDaysMissed(thisStudent);
+                    };
+                })());
+            }
+            var daysMissed = document.createElement('td') //creates text for days missed
+            daysMissed.appendChild(document.createTextNode(students[i].daysMissed));
+            tr.appendChild(daysMissed);
+            tbdy.appendChild(tr); // add each student row to the table body
         }
-        tbl.appendChild(tbdy);//add the table body to the table
-        tayTableDiv.appendChild(tbl);//add the table to the tayTableDiv
+
+        tbl.appendChild(tbdy); //add the table body to the table
+        tayTableDiv.appendChild(tbl); //add the table to the tayTableDiv
     }
-};
+}
 
 
 octo.init();
-view.render();
